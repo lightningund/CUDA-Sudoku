0cbfcccd773f592fffde5b3da4da455db6c73534
lightningund <lightningundies0826@gmail.com>
2024-06-23 11:06:22 -0500

    debugging

--- main.cu
+++ main.cu
@@ -1,41 +1,43 @@
 #include <array>
 #include <iostream>
-#include <bitset>
 #include <vector>
-#include <functional>
+
+#define CUCHECK(str, x) printf(str ": %d\n", (x))
 
 constexpr size_t board_size = 9;
 constexpr size_t total_board = board_size * board_size;
 
-// struct Rule {
-// 	std::function<std::vector<Vec2>(const Vec2&)> get_cells;
-// 	std::function<bool(const std::vector<uint8_t>&)> is_valid;
-// };
+struct Rule {
+	uint2* (*get_cells)(const uint2);
+	bool (*is_valid)(const uint8_t*);
+};
 
-// using state_set = std::bitset<board_size>;
+// Checks for no duplicate cells. Used a lot so I just put it here
+__device__
+bool default_rule(const uint8_t* cell_vals) {
+	uint16_t states = 0;
 
-// // Checks for no duplicate cells. Used a lot so I just put it here
-// bool default_rule(const std::vector<uint8_t>& cell_vals) {
-// 	state_set states{};
+	for (int i = 0; i < board_size; ++i) {
+		states |= 1 << cell_vals[i];
+	}
 
-// 	for (auto& val : cell_vals) {
-// 		states.set(val);
-// 	}
+	return states == ((1 << board_size) - 1);
+};
 
-// 	return states.count() == cell_vals.size();
-// };
+__device__
+const Rule column_rule = {
+	[](const uint2 pos) -> uint2* {
+		uint2* cells;
+		CUCHECK("Allocating", cudaMalloc(&cells, board_size));
+		for (unsigned int i = 0; i < board_size; ++i) {
+			cells[i] = uint2{pos.x, i};
+		};
 
-// const std::vector<Rule> rules = {
-// 	{ // Columns
-// 		[](const Vec2& pos) -> std::vector<Vec2> {
-// 			std::vector<Vec2> cells{};
-// 			for (int i{0}; i < BOARD_SIZE; i++) {
-// 				if (i != pos.y) cells.push_back(Vec2{pos.x, i});
-// 			};
+		return cells;
+	},
+	default_rule
+};
 
-// 			return cells;
-// 		}, default_rule
-// 	},
 // 	{ // Rows
 // 		[](const Vec2& pos) -> std::vector<Vec2> {
 // 			std::vector<Vec2> cells{};
@@ -64,34 +66,74 @@ constexpr size_t total_board = board_size * board_size;
 using Board = std::array<uint8_t, total_board>;
 
 __device__
-bool check_state(size_t index, Board& board) {
-	return true;
+bool check_state(size_t index, uint8_t* board) {
+	uint2 pos{(uint16_t)(index % board_size), (uint16_t)(index / board_size)};
+	printf("x: %u, y: %u\n", pos.x, pos.y);
+	auto cells = column_rule.get_cells(pos);
+	uint8_t vals[board_size];
+
+	for (int i = 0; i < board_size; ++i) {
+		vals[i] = board[cells[i].x + cells[i].y * board_size];
+	}
+
+	CUCHECK("Attempting to free", cudaFree(cells));
+	printf("First Val: %hu\n", vals[0]);
+
+	return column_rule.is_valid(vals);
 }
 
 __device__
-bool check_cell(size_t index, Board& board) {
+bool check_cell(size_t index, uint8_t* board) {
 	if (index > total_board) return true;
 	if (board[index] != 255) {
 		return check_cell(index + 1, board);
 	}
 
-	for (int i = 0; i < board_size; ++i) {
+	printf("Gonna check %lu, val: %hu\n", index, board[index]);
+
+	for (uint8_t i = 0; i < (uint8_t)board_size; ++i) {
+		printf("Checking val %hu\n", i);
 		board[index] = i;
 
 		if (check_state(index, board)) {
+			printf("Cell %lu works with val %hu\n", index, i);
 			if (check_cell(index + 1, board)) {
 				return true;
 			}
+		} else {
+			printf("%hu no worky\n", i);
 		}
 	}
+
+	printf("welp done\n");
 
 	board[index] = 255;
 	return false;
 }
 
+__device__
+void dev_print_board(uint8_t* board) {
+	for (int i = 0; i < 9; ++i) {
+		for (int j = 0; j < 9; ++j) {
+			uint8_t cell = board[i * 9 + j];
+			if (cell == 255) {
+				printf("_ ");
+			} else {
+				printf("%u ", cell + 1);
+			}
+		}
+
+		printf("\n");
+	}
+
+	printf("\n");
+}
+
 __global__
-void solve(Board* board) {
-	check_cell(0, *board);
+void solve(uint8_t* board) {
+	printf("Gonna solve it\n");
+	dev_print_board(board);
+	check_cell(0, board);
 }
 
 void print_board(Board& board) {
@@ -154,16 +196,16 @@ int main() {
 		loop++;
 	}
 
-	Managed<Board> dev_board{81};
-	cudaMemcpy(dev_board.raw, &board, 81, cudaMemcpyHostToDevice);
+	Managed<uint8_t> dev_board{81};
+	CUCHECK("Attempting to copy", cudaMemcpy(dev_board.raw, board.data(), 81, cudaMemcpyHostToDevice));
 
 	print_board(board);
 
 	solve<<<1, 1, 1>>>(dev_board.raw);
 
-	cudaDeviceSynchronize();
+	CUCHECK("Attempting to sync", cudaDeviceSynchronize());
 
-	cudaMemcpy(board.data(), dev_board.raw, 81, cudaMemcpyDeviceToHost);
+	CUCHECK("Attempting to copy back", cudaMemcpy(board.data(), dev_board.raw, 81, cudaMemcpyDeviceToHost));
 
 	print_board(board);
 
